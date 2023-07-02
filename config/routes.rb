9fb7ae06fe91e7766d6d5ae51d6af808fd326d58
kouki515 <71614240+kouki515@users.noreply.github.com>
2023-07-03 05:17:03 +0900

    complete tutorial

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :microposts
+  resources :users
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-  root 'application#hello'
+  root 'users#index'
 end
